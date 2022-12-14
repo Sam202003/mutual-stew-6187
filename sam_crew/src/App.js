fa619dd9db35b7a899e4f327c749aa723718cd2b
Sam202003 <103636116+Sam202003@users.noreply.github.com>
2022-12-14 21:09:44 +0530

    added

--- sam_crew/src/App.js
+++ sam_crew/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from 'react-redux'
+import './App.css'
+import MainModal from './Components/MainModal/MainModal'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { isOpen } = useSelector((store) => store.modal)
+  return <div className="App">{isOpen && <MainModal />}</div>
 }
 
-export default App;
+export default App
